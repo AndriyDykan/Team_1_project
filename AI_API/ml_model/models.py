b8de4ad3e33efc1a0e4571e230c61c295334e32b
AndriyDykan <130219597+AndriyDykan@users.noreply.github.com>
2024-03-16 09:38:25 +0200

    updated css

--- AI_API/ml_model/models.py
+++ AI_API/ml_model/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+from .model.model import predict_image
+import os
+from PIL import Image as PilImage
+from io import BytesIO
 
 class Image(models.Model):
     class_choices = [
-        (1, 'airplane'),
-        (2, 'automobile'),
-        (3, 'bird'),
-        (4, 'cat'),
-        (5, 'deer'),
-        (6, 'dog'),
-        (7, 'frog'),
-        (8, 'horse'),
-        (9, 'ship'),
-        (10, 'truck'),
+        (0, 'airplane'),
+        (1, 'automobile'),
+        (2, 'bird'),
+        (3, 'cat'),
+        (4, 'deer'),
+        (5, 'dog'),
+        (6, 'frog'),
+        (7, 'horse'),
+        (8, 'ship'),
+        (9, 'truck'),
     ]
     image_class = models.IntegerField(choices=class_choices)
     image_predicted_class = models.IntegerField(choices=class_choices)
@@ -21,9 +25,14 @@ class Image(models.Model):
         return f"{self.get_image_class_display()} - {self.id}"
 
     def save(self, *args, **kwargs):
+        # Створення копії фотографії
+        copied_image = self.image
+
         # Код для передбачення класу зображення
 
-        self.image_predicted_class = 1
 
-        # Виклик оригінального методу save
+        # Передача копії фотографії у функцію для передбачення класу зображення
+        self.image_predicted_class = predict_image(copied_image)
+
+        # Збереження оригінальної форми з початковим зображенням
         super().save(*args, **kwargs)
--- AI_API/ml_model/views.py
+++ AI_API/ml_model/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .form import ImageForm
 from .models import Image
 
-from .model.model import prediction
+
 
 #def index(request):
 #    if request.method == "POST":
@@ -26,10 +26,12 @@ def index(request):
         if form.is_valid():
             form.save()
             latest_image = Image.objects.latest('id')
-            return render(request, "index.html", {"form": form, "latest_image": latest_image})
+            return render(request, "index.html", {"form": form, "latest_image": latest_image, 'image_class_choices': Image.class_choices})
     else:
-        return render(request, "index.html")
+        form = ImageForm()
+    img = Image.objects.all()
+    return render(request, "index.html", {"img": img, "form": form,'image_class_choices': Image.class_choices})
 
 def results_page(request):
-    results = Image.objects.all()  # Отримання всіх результатів з бази даних
+    results = Image.objects.all().order_by('-id')
     return render(request, 'results.html', {'img': results})
